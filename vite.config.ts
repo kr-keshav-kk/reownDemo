1d5d7ff9f686c2bc618b8065be4c5b32ea7896a3
Keshav <keshavk@buyhatke.com>
2025-08-21 16:25:21 +0530

    chore: node polyfills added

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
   plugins: [sveltekit()],
@@ -14,5 +16,17 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
+  },
+
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodePolyfills({ include: ["crypto", "http"] }),
+        inject({
+          Buffer: ["buffer/", "Buffer"],
+          process: "process/browser",
+        }),
+      ],
+    },
   },
 });
